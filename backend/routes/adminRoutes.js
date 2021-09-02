3e54ab695fb94904df0c7bd133af4a2ed9df233d
Stark <starlefsol1@gmail.com>
2021-09-02 14:04:44 +0300

    trash

--- backend/routes/adminRoutes.js
+++ backend/routes/adminRoutes.js
@@ -5,6 +5,5 @@ const adminController = require('../controllers/adminController.js');
 router.post('/questions/add', adminController.addQuestion);
 router.post('/testStages/add', adminController.addTestStage);
 router.post('/tests/add', adminController.addTest);
-router.post('/tests/file', adminController.addTestFromFile);
 
 module.exports = router;
